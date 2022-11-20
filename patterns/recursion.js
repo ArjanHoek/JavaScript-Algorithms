383b007e696e02e77bfe84d65a35d0a848199dba
Arjan Hoek <arjanhoek5191@gmail.com>
2022-11-20 12:01:08 +0100

    add first set of exercises

--- patterns/recursion.js
+++ patterns/recursion.js
@@ -3,24 +3,43 @@ const countDown = n => {
   return n && countDown(n - 1);
 };
 
-// countDown(3);
-
 const sumRange = n => (n ? n-- + sumRange(n) : n);
-
-// console.log(sumRange(5));
 
 const hasOdds = arr =>
   !arr.length ? false : !(arr[0] % 2) ? hasOdds(arr.slice(1)) : true;
-
-console.log(hasOdds([2, 8, 10]));
-
-const factorial = n => (n === 1 ? n : n * factorial(n - 1));
-
-console.log(factorial(4));
 
 const collectOdds = (arr, res = []) =>
   !arr.length
     ? res
     : collectOdds(arr.slice(1), arr[0] % 2 ? [...res, arr[0]] : res);
 
-console.log(collectOdds([1, 2, 5, 2, 5, 7, 8, 7, 19, 4, 5]));
+// CHALLENGES
+
+// 1: Power
+const power = (base, exp) => (!exp ? 1 : base * power(base, exp - 1));
+
+// 2: Factorial
+const factorial = n => (!n ? 1 : n * factorial(n - 1));
+
+// 3: Product of numbers in array
+const productOfArray = arr =>
+  arr.length <= 1
+    ? arr[0] || 0
+    : productOfArray([arr[0] * arr[1], ...arr.slice(2)]);
+
+// 4: Sum of all numbers between 0 and a given number
+const recursiveRange = num => (!num ? 0 : num + recursiveRange(num - 1));
+
+// 5: Fibonacci
+const fib = num => {
+  if (num <= 2) return 1;
+
+  const calculate = (prev = 1, next = 1, iteration = 3) =>
+    iteration === num
+      ? prev + next
+      : calculate(next, prev + next, iteration + 1);
+
+  return calculate();
+};
+
+console.log(fib(5));
